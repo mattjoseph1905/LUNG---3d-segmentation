1d3af16f473632b33e96b0766c0d090c7499b9cd
mattjoseph1905 <81772654+mattjoseph1905@users.noreply.github.com>
2024-07-05 16:39:33 +0100

    Add files via upload

---
+++ src/data_module.py
@@ -0,0 +1,171 @@
+import lightning as L
+from monai.apps import DecathlonDataset
+from monai.transforms import (
+    Compose,
+    CropForegroundd,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    LoadImaged,
+    NormalizeIntensityd,
+    Orientationd,
+    RandFlipd,
+    Resized,
+    ScaleIntensityRanged,
+    Spacingd,
+)
+from torch.utils.data import DataLoader
+
+
+class DecathlonDataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        root_dir: str,
+        task: str,
+        batch_size: int,
+        num_workers: int,
+        seed: int = 42,
+    ):
+        super().__init__()
+        self.root_dir = root_dir
+        self.task = task
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.seed = seed
+
+    def setup(self, stage=None):
+        train_transform = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                EnsureChannelFirstd(keys="image"),
+                EnsureTyped(keys=["image", "label"]),
+                Orientationd(keys=["image", "label"], axcodes="RAS"),
+                # Spacingd(
+                #     keys=["image", "label"],
+                #     pixdim=(1.0, 1.0, 1.0),
+                #     mode=("bilinear", "nearest"),
+                # ),
+                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+                # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+                # CropForegroundd(keys=["image", "label"], source_key="image"),
+                Resized(
+                    keys=["image", "label"],
+                    spatial_size=(128, 128, 128),
+                ),
+            ]
+        )
+        val_transform = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                EnsureChannelFirstd(keys=["image", "label"]),
+                EnsureTyped(keys=["image", "label"]),
+                Orientationd(keys=["image", "label"], axcodes="RAS"),
+                # Spacingd(
+                #     keys=["image", "label"],
+                #     pixdim=(1.5, 1.5, 2.0),
+                #     mode=("bilinear", "nearest"),
+                # ),
+                ScaleIntensityRanged(
+                    keys=["image"],
+                    a_min=-57,
+                    a_max=164,
+                    b_min=0.0,
+                    b_max=1.0,
+                    clip=True,
+                ),
+                # CropForegroundd(keys=["image", "label"], source_key="image"),
+                Resized(keys=["image", "label"], spatial_size=(128, 128, 128)),
+            ]
+        )
+        transform = Compose(
+            [
+                LoadImaged(keys=["image", "label"]),
+                EnsureChannelFirstd(keys=["image", "label"]),
+                ScaleIntensityRanged(
+                    keys=["image"],
+                    a_min=-175,
+                    a_max=250,
+                    b_min=0.0,
+                    b_max=1.0,
+                    clip=True,
+                ),
+                CropForegroundd(keys=["image", "label"], source_key="image"),
+                Orientationd(keys=["image", "label"], axcodes="RAS"),
+                Spacingd(
+                    keys=["image", "label"],
+                    pixdim=(1.5, 1.5, 2.0),
+                    mode=("bilinear", "nearest"),
+                ),
+                Resized(keys=["image", "label"], spatial_size=(128, 128, 128)),
+            ]
+        )
+
+        self.train_data = DecathlonDataset(
+            root_dir="C:/Users/matij/Downloads/lung-tumours-segmentation-main/data",
+            task=self.task,
+            transform=transform,
+            section="training",
+            seed=self.seed,
+            download=True,
+            cache_rate=0.0,
+        )
+
+        self.val_data = DecathlonDataset(
+            root_dir="C:/Users/matij/Downloads/lung-tumours-segmentation-main/data",
+            task=self.task,
+            transform=transform,
+            section="validation",
+            seed=self.seed,
+            download=False,
+            cache_rate=0.0,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_data,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_data,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+
+if __name__ == "__main__":
+    data_module = DecathlonDataModule(
+        root_dir="./data",
+        task="Task06_Lung",
+        batch_size=1,
+        num_workers=10,
+    )
+    data_module.setup()
+    train_loader = data_module.train_dataloader()
+    val_loader = data_module.val_dataloader()
+    print("Count", len(train_loader), len(val_loader))
+
+    batch = next(iter(val_loader))
+    x, y = batch["image"], batch["label"]
+    print("Shape", x.shape, y.shape)
+
+    from monai.networks.nets import UNet
+    from monai.networks.layers import Norm
+
+    unet = UNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=1,
+        channels=(16, 32, 64, 128),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.BATCH,
+    ).to("cuda")
+
+    y_hat = unet(x.to("cuda"))
+
+    print(y_hat.shape)
---
+++ src/model.py
@@ -0,0 +1,88 @@
+import lightning as L
+import torch
+from monai.losses import DiceLoss
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.metrics import compute_generalized_dice
+from monai.inferers import sliding_window_inference
+
+
+class DecathlonModel(L.LightningModule):
+    def __init__(
+        self,
+        learning_rate: float = 3e-4,
+        use_scheduler: bool = True,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.learning_rate = learning_rate
+        self.use_scheduler = use_scheduler
+
+        # Define the model
+        self._model = UNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=1,
+            channels=(32, 64, 128, 256),
+            strides=(2, 2, 2, 2),
+            num_res_units=3,
+            norm=Norm.BATCH,
+        )
+
+        # Define the loss function
+        self.criterion = DiceLoss(to_onehot_y=True, softmax=True)
+
+    def forward(self, x):
+        return self._model(x)
+
+    def training_step(self, batch):
+        x, y = batch["image"], batch["label"]
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        return loss
+
+    # define inference method
+    def _inference(self, input):
+        def _compute(input):
+            return sliding_window_inference(
+                inputs=input,
+                roi_size=(128, 128, 128),
+                sw_batch_size=1,
+                predictor=self,
+                overlap=0.5,
+            )
+
+        VAL_AMP = True
+        if VAL_AMP:
+            with torch.cuda.amp.autocast():
+                return _compute(input)
+        else:
+            return _compute(input)
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["image"], batch["label"]
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        y_pred = self._inference(x)
+        dice = compute_generalized_dice(y_pred, y)
+        dice = dice.mean() if len(dice) > 0 else dice
+        self.log("val_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("val_dice", dice, on_step=True, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.parameters(), lr=self.learning_rate, weight_decay=0.05
+        )
+
+        configuration = {
+            "optimizer": optimizer,
+            "monitor": "val_loss",  # monitor validation loss
+        }
+
+        if self.use_scheduler:
+            # Add lr scheduler
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20)
+            configuration["lr_scheduler"] = scheduler
+
+        return configuration
